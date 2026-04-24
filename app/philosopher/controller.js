69ab71670b583ed8e13d147163738281aaf73c72
achmadfauzanhusain <achmadfauzan1340@gmail.com>
2026-04-25 01:02:15 +0700

    fix structure of json

--- app/philosopher/controller.js
+++ app/philosopher/controller.js
@@ -1,7 +1,12 @@
+const { philosophers } = require("../../json/philosopher.json")
+
 module.exports = {
     getAllPhilosophers: async(req, res) => {
         try {
-            
+            res.status(200).json({
+                message: "success!",
+                data: philosophers
+            })
         } catch (error) {
             res.status(500).json({ message: "Internal server error" });
         }
