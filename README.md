91b300b6f003cdbd625a2d8f92d12e13b6c3c5b4
achmadfauzanhusain <achmadfauzan1340@gmail.com>
2026-04-24 13:08:31 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Philosopher-API
+mode=development
---
+++ app/test/controller.js
@@ -0,0 +1,13 @@
+module.exports = {
+    test: async(req, res) => {
+        try {
+            res.status(200).json({
+                message: "Hello World"
+            });
+        } catch (error) {
+            res.status(500).json({
+                message: "Internal Server Error"
+            });
+        }
+    }
+}
